d72dfa102d77cb42d686ba3299ce339a9e739f94
raka <bimandugal@gmail.com>
2022-01-07 15:20:03 +0700

    Minor Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ import "./Style.css";
 const Footer = () => {
     return (
         <div class="footer">
-            <a href="#">
+            <a href="https://www.facebook.com">
                 <i class="fa fa-facebook-square fa-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://twitter.com">
                 <i class="fa fa-twitter-square fa-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://id.pinterest.com">
                 <i class="fa fa-pinterest-square fa-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://www.linkedin.com">
                 <i class="fa fa-linkedin-square fa-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://www.youtube.com">
                 <i class="fa fa-youtube-square fa-2x"></i>
             </a>
             <p>Supreme Store by Bima Rakajati &copy; 2021.</p>
